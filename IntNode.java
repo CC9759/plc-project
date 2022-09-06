61cde35314af96fb047bc92e227bc0a67528c98f
Celina C <cc9759@rit.edu>
2022-09-06 12:15:08 -0400

    implemented intNode function

--- IntNode.java
+++ IntNode.java
@@ -1,3 +1,33 @@
 public class IntNode implements JottTree {
+    private final Token node;
+    private final int value;
     
+    public IntNode(Token node){
+        this.node = node;
+        value = Integer.parseInt(node.getToken());
+    }
+
+    public int getValue(){
+        return value;
+    }
+
+    public String convertToJott(){
+        return node.getToken();
+    }
+
+    public String convertToJava(){
+        return null;
+    };
+
+    public String convertToC(){
+        return null;
+    };
+
+    public String convertToPython(){
+        return null;
+    };
+
+    public boolean validateTree(){
+        return false;
+    };
 }
