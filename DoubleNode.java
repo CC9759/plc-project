4c4ef257be2c9a29abbf3faf25e226addd90f518
hmasaryk <hgm2940@rit.edu>
2022-09-07 15:17:38 -0400

    Made a class for the double node

--- DoubleNode.java
+++ DoubleNode.java
@@ -1,2 +1,42 @@
-public class DoubleNode {
+public class DoubleNode implements JottTree{
+    private final Token node;
+
+    private final double value;
+
+
+    public DoubleNode(Token node){
+        this.node = node;
+        this.value = Double.parseDouble(node.getToken());
+    }
+
+    public double getValue(){
+        return this.value;
+    }
+
+    public String convertToJott(){
+        return this.node.getToken();
+    }
+
+    public String convertToJava(){
+        return null;
+    }
+
+    public String convertToC(){
+        return null;
+    }
+
+    public String convertToPython(){
+        return null;
+    }
+
+    public boolean validateTree(){
+        return false;
+    }
+
+
+
+
+
+
+
 }
