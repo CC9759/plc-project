0aad55436b25b9ca033f55d51d7a01e826f6773c
Kaiming  Zhang <kz3640@cs.rit.edu>
2022-09-08 11:40:55 -0400

    initial commit

---
+++ ConstantNode.java
@@ -0,0 +1,46 @@
+public class ConstantNode implements JottTree {
+    private Token constant;
+    private TokenType type;
+
+    /**
+     * Will output a string of this tree in Jott
+     * @return a string representing the Jott code of this tree
+     */
+    public String convertToJott() {
+        return null;
+    }
+
+    /**
+     * Will output a string of this tree in Java
+     * @return a string representing the Java code of this tree
+     */
+    public String convertToJava() {
+        return null;
+    }
+
+    /**
+     * Will output a string of this tree in C
+     * @return a string representing the C code of this tree
+     */
+    public String convertToC() {
+        return null;
+    }
+
+    /**
+     * Will output a string of this tree in Python
+     * @return a string representing the Python code of this tree
+     */
+    public String convertToPython() {
+        return null;
+    }
+
+    /**
+     * This will validate that the tree follows the semantic rules of Jott
+     * Errors validating will be reported to System.err
+     * @return true if valid Jott code; false otherwise
+     */
+    public boolean validateTree() {
+        return true;
+    }
+
+}
