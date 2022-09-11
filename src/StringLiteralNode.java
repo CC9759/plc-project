8806f2c29e6987285d2cbc5c97d4631a752a8787
kz3640 <kz3640@g.rit.edu>
2022-09-11 14:09:29 -0400

    Created StringLiteralNode for string literals

---
+++ src/StringLiteralNode.java
@@ -0,0 +1,54 @@
+public class StringLiteralNode implements JottTree {
+
+    private ConstantNode child;
+
+    /**
+     * Will output a string of this tree in Jott
+     * @return a string representing the Jott code of this tree
+     */
+    public StringLiteralNode(ConstantNode inputNode) {
+        this.child = inputNode;
+    }
+
+    public String convertToJott() {
+        return "\"" + this.child.toString() + "\"";
+    }
+
+    /**
+     * Will output a string of this tree in Java
+     * @return a string representing the Java code of this tree
+     */
+    public String convertToJava() {
+        return "\"" + this.child.toString() + "\"";
+    }
+
+    /**
+     * Will output a string of this tree in C
+     * @return a string representing the C code of this tree
+     */
+    public String convertToC() {
+        return "\"" + this.child.toString() + "\"";
+    }
+
+    /**
+     * Will output a string of this tree in Python
+     * @return a string representing the Python code of this tree
+     */
+    public String convertToPython() {
+        return "\"" + this.child.toString() + "\"";
+    }
+
+    /**
+     * This will validate that the tree follows the semantic rules of Jott
+     * Errors validating will be reported to System.err
+     * @return true if valid Jott code; false otherwise
+     */
+    public boolean validateTree() {
+        return true;
+    }
+
+    public String toString() {
+        return "\"" + this.child.toString() + "\"";
+    }
+
+}
