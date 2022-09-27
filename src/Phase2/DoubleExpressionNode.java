903c7fb7996b7280f826356d3b23bf7228a29ebf
kz3640 <kz3640@g.rit.edu>
2022-09-27 11:46:22 -0400

    DoubleExpressionNode changed to create nodes using lists

--- src/Phase2/DoubleExpressionNode.java
+++ src/Phase2/DoubleExpressionNode.java
@@ -7,11 +7,9 @@ public class DoubleExpressionNode extends ExpressionNode {
         Token firstToken = inputTokens.get(0);
         if(firstToken.getTokenType() == TokenType.ID_KEYWORD) {
             this.children.add(new IDKeywordNode(inputTokens));
-            inputTokens.remove(0);
         }
         else if(firstToken.getTokenType() == TokenType.NUMBER) {
             this.children.add(new ConstantNode(inputTokens));
-            inputTokens.remove(0);
         }
         if (inputTokens.size() > 1) {
             Token secondToken = inputTokens.get(0);
@@ -20,16 +18,13 @@ public class DoubleExpressionNode extends ExpressionNode {
                     "+-/*".contains(secondToken.getToken()) &&
                     isValidStart(thirdToken)) {
                 this.children.add(new RelOpNode(inputTokens));
-                inputTokens.remove(0);
                 this.children.add(new DoubleExpressionNode(inputTokens));
             }
         }
     }
     public static boolean isValidStart(Token thisToken) {
         if (thisToken.getTokenType() == TokenType.NUMBER) {
-            if (thisToken.getToken().contains(".")) {
-                return true;
-            }
+            return thisToken.getToken().contains(".");
         }
         return thisToken.getTokenType() == TokenType.ID_KEYWORD;
     }
