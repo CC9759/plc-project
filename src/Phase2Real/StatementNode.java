9183633a7f21cba00b4de7a6cd0a5df8d2a476d7
hmasaryk <hgm2940@rit.edu>
2022-10-02 16:11:01 -0400

    made functions abstract

--- src/Phase2Real/StatementNode.java
+++ src/Phase2Real/StatementNode.java
@@ -25,27 +25,34 @@ public abstract class StatementNode implements JottTree{
     }
 
     /**
+     * Will output a string of this tree in Jott
+     * @return a string representing the Jott code of this tree
+     */
+    public abstract String convertToJott();
+
+
+    /**
      * Will output a string of this tree in Java
      * @return a string representing the Java code of this tree
      */
-    public String convertToJava(){return null;}
+    public abstract String convertToJava();
 
     /**
      * Will output a string of this tree in C
      * @return a string representing the C code of this tree
      */
-    public String convertToC(){return null;}
+    public abstract String convertToC();
 
     /**
      * Will output a string of this tree in Python
      * @return a string representing the Python code of this tree
      */
-    public String convertToPython(){return null;}
+    public abstract String convertToPython();
 
     /**
      * This will validate that the tree follows the semantic rules of Jott
      * Errors validating will be reported to System.err
      * @return true if valid Jott code; false otherwise
      */
-    public boolean validateTree(){return false;}
+    public abstract boolean validateTree();
 }
