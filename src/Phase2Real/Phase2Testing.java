985f953a57fedf5e815894ecde0c3f03f869279e
jrl1373 <jrl1373@rit.edu>
2022-10-05 17:33:17 -0400

    throws exception

--- src/Phase2Real/Phase2Testing.java
+++ src/Phase2Real/Phase2Testing.java
@@ -6,7 +6,7 @@ import java.beans.Expression;
 import java.util.*;
 
 public class Phase2Testing {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         Scanner in = new Scanner(System.in);
         System.out.println("Input test case:");
         System.out.println("1: ConstantNode");
@@ -16,7 +16,6 @@ public class Phase2Testing {
         System.out.println("5: ExpressionNode_Integer");
         System.out.println("6: ExpressionNode_String");
         System.out.println("7: FunctionDefNode");
-        System.out.println("8: Phase 2 test cases");
 
         String input = in.nextLine();
 
@@ -95,20 +94,13 @@ public class Phase2Testing {
                 testList.add(new Token("11", TokenType.NUMBER));
                 ExpressionNode testExpressionIntegerNode = ExpressionNode.parseExpressionNode(testList);
                 System.out.println(testExpressionIntegerNode.convertToJott());
-                break;
             case 6:
 
             case 7:
                 System.out.println("testing on the function definition in phase1Example");
-                ArrayList<Token> tokens = JottTokenizer.tokenize("C:\\Users\\15164\\IdeaProjects\\plc-project\\tokenizerTestCases\\phase1Example.jott");
+                ArrayList<Token> tokens =  JottTokenizer.tokenize("parserTestCases/whileKeyword.jott");
                 ProgramNode result = ProgramNode.parseProgramNode(tokens);
                 System.out.println(result.convertToJott());
-                break;
-            case 8:
-                ArrayList<Token> tokens2 = JottTokenizer.tokenize("C:\\Users\\15164\\IdeaProjects\\plc-project\\parserTestCases\\providedExample5.jott");
-                ProgramNode result2 = ProgramNode.parseProgramNode(tokens2);
-                System.out.println(result2.convertToJott());
-
         }
     }
 }
