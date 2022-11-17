7a989bc30ed43abf3154b68171e2a0abfcd2d4df
Celina C <cc9759@rit.edu>
2022-11-17 14:41:25 -0500

    Completed final Jott file

--- src/Jott.java
+++ src/Jott.java
@@ -1,25 +1,42 @@
 import java.io.*;
 import java.util.*;
+
 import Phase1.*;
+import Phase2.*;
 
 public class Jott {
     public static void main(String args[]) {
-        String inputName = args[2];
-        String outputName = args[3];
-        String language = args[4];
+        String inputName = args[1];
+        String outputName = args[2];
+        String language = args[3];
+        String result = "";
 
-        File input = new File(inputName);
-        File output = new File(outputName);
+        ArrayList<Token> tokens =  JottTokenizer.tokenize(inputName);
+        ProgramNode root = ProgramNode.parseProgramNode(tokens);
 
-        String data = "";
+        switch(language){
+            case "Java":
+                result = root.convertToJava();
+                result.replaceFirst("Main", outputName.split(".")[0]);
+                break;
+            case "Python":
+                result = root.convertToPython();
+                break;
+            case "C":
+                result = root.convertToC();
+                break;
+            case "Jott":
+                result = root.convertToJott();
+                break;
+        }
 
         try {
-            Scanner myReader = new Scanner(input);
-            while(myReader.hasNextLine()) {
-                data += myReader.nextLine();
-            }
-        } catch (FileNotFoundException e) {
+            BufferedWriter f_writer = new BufferedWriter(new FileWriter(outputName));
+            f_writer.write(result);
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
+        System.out.println();
     }
 }
