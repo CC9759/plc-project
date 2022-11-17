6da7b7bfe6468a0aab6b8a807aca643979871a35
hmasaryk <hgm2940@rit.edu>
2022-11-17 12:03:13 -0500

    fixed issue

--- src/Phase2/FunctionDefNode.java
+++ src/Phase2/FunctionDefNode.java
@@ -121,7 +121,8 @@ public class FunctionDefNode implements JottTree{
 
     @Override
     public boolean validateTree() {
-        if(!returnable() && returnType!=InformationType.VOID){return false;}
+        if(returnable() ==null && returnType!=InformationType.VOID){return false;}
+        if(returnable() !=null && returnType==InformationType.VOID){return false;}
         return (this.myIDKeywordNode.validateTree() && this.myFunctionDefParamsNode.validateTree() &&
         this.myReturnNode.validateTree() && this.myBodyNode.validateTree());
     }
